ea4abb9e573fc37d2acb104de95e964fa5a417a4
sr.bala <sr.bala.kkl@gmail.com>
2023-09-18 11:32:39 +0530

    new:initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
